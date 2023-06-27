5c4db240c65d4fe403b271fcd7a7faad8683416a
byMagg <byMag@hotmail.es>
2023-06-27 12:21:08 +0200

    update

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -4,13 +4,15 @@ export class Project {
     year: string | undefined;
     frameworks: Framework[] | undefined;
     image: string | undefined;
+    url?: string | undefined;
 
-    constructor(name: string, description: string, year: string, frameworks: Framework[], image: string) {
+    constructor(name: string, description: string, year: string, frameworks: Framework[], image: string, url?: string) {
         this.name = name;
         this.description = description;
         this.year = year;
         this.frameworks = frameworks;
         this.image = image;
+        this.url = url;
     }
 }
 
